0220033d51204b15614d94e66eef9d3f8c14cfc1
NeytChi <neyton61@gmail.com>
2019-12-26 15:34:44 +0200

    learning arguments command line: polish calculation.

--- C-ANSI/arguments-command-line/expr_c.c
+++ C-ANSI/arguments-command-line/expr_c.c
@@ -13,12 +13,9 @@ double pop(void);
 
 int sp = 0;
 double val[MAXVAL];
-char buf[BUFSIZE];
-int bufp = 0;
 short negative = 0;
-short single = 0;
 
-int getch(void)
+/*int getch(void)
 {
     return (bufp > 0) ? buf[--bufp] : getchar();
 }
@@ -28,7 +25,7 @@ void ungetch(int c)
         printf("ungetch: too many characters\n");
     else
         buf[bufp++] = c;
-}
+}*/
 void push(double f)
 {
     if (sp < MAXVAL)
@@ -48,10 +45,9 @@ double pop(void)
 {
     if (sp > 0)
         return val[--sp];
-    else {
+    else 
         printf("error: stack empty\n");
-        return 0.0;
-    }
+    return 0.0;
 }
 int isdigit(char s)
 {
@@ -62,14 +58,19 @@ int isdigit(char s)
 }
 int getop(char s[])
 {
+    printf("parameter - %s\n", s);
     int i, c;
 
-    while ((s[0] = c = getch()) == ' ' || c == '\t')
-        ;
-    s[1] = '\0';
-    if (!isdigit(c) && c != '.' && c != '-')
-        return c;
     i = 0;
+    
+    while ((c = s[i++]) == ' ' || c == '\t')
+        ;
+    
+    if (!isdigit(c) && c != '.' && c != '-')
+    {
+        s[i] = '\0';
+        return c;
+    }
     if (c == '-')
     {
         --i;
@@ -77,70 +78,84 @@ int getop(char s[])
         negative = 1;
     }
     if (isdigit(c))
-        while (isdigit(s[++i] = c = getch()))   
+        while (isdigit(c = s[i++]))   
             ;
     if (c == '.')
-        while(isdigit(s[++i] = c = getch()))
+        while(isdigit(c = s[i++]))
             ;
-    s[i] = '\0';
-    if (c != EOF)
-        ungetch(c);
+    //s[i] = '\0';
+    //if (c != EOF)
+        //ungetch(c);
     return NUMBER;
 }
-void helps()
+void helps(char s[])
 {
-    printf("expr options:\n");
-    printf("[number-1][number-N][operation-1][operation-N]\n");
-    printf("available operations: - '+' '-' '*' '/' '%c' 's'(sin) 'e'(exp) 'p'(pow))\n", '%');
+    if (s[0] == '-' && s[1] == 'h')
+    {
+        printf("expr options:\n");
+        printf("[number-1] [number-N] [operation-1] [operation-N]\n");
+        printf("available operations: - '+' '-' '*' '/' '%c')\n", '%');
+    }
+}
+void getresult()
+{
+    double v = pop();
+    printf("\t%.8g\n", v);
+    push(v);
 }
 int main(int argc, char *argv[])
 {
     int type, i;
     double op2;
     char s[MAXOP];
-
-    i = 0;
-    helps();
-    while((type = getop(argv[i++])) < argc) {
+    
+    i = 1;
+    if (argc >= i) 
+        helps(argv[i]);
+    while(i < argc) {
+        type = getop(argv[i]);
         switch(type) {
             case NUMBER:
-                push(atof(s));
-                single = 1;
+                push(atof(argv[i]));
                 break;
             case '+':
                 push(pop() + pop());
+                getresult();
                 break;
-            case '*':
+            case 'x':
                 push(pop() * pop());
+                getresult();
                 break;
             case '-':
                 op2 = pop();
                 push(pop() - op2);
+                getresult();
                 break;
             case '/':
                 op2 = pop();
                 if (op2 != 0.0)
+                {
                     push(pop() / op2);
+                    getresult();
+                }
                 else
                     printf("error: zero divisor.\n");
                 break;
             case '%':
                 op2 = pop();
                 if (op2 != 0.0)
+                {
                     push(((int)pop()) % ((int)op2));
+                    getresult();
+                }
                 else
                     printf("error: zero divisor.\n");
-                break;
-            case '\n':
-                if (single == 0)
-                    printf("\t%.8g\n", pop());
-                else
-                    single = 0;
                 break;
             default:
                 printf("error: unknown command %s\n", s);
                 break;
         }
+        ++i;
     }
     return 0;
 }
